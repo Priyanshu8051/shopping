7feac5d2993130f9183d6629a78ac5a8d3161365
priyanshu kumar prince <priyanshu kumar>
2021-08-14 03:38:20 +0530

    my first commit

---
+++ index.php
@@ -0,0 +1,464 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>FashiohTrendS</title>
+	<link rel="stylesheet" type="text/css" href="css/fashion.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="mega_menu.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+
+
+</head>
+<style type="text/css">
+	.hov{
+		header.hover
+	}
+	
+</style>
+<body class="hov">
+	<div class="header">
+		
+		
+	</div>
+	<style type="text/css">
+		.marg1{
+			margin-left:500px;
+		}
+		
+	</style>
+
+	<div class="navmenu ">
+		<ul>
+			<li><a class="mynav marg1" href="#">men</a>
+				<ul class="megamenu">
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+					
+				</ul>
+
+
+			</li>
+
+
+
+
+			<li><a class="mynav" href="#">women</a>
+				<ul class="megamenu">
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+					
+				</ul>
+
+
+			</li>
+			<li><a class="mynav" href="#">kid</a>
+				<ul class="megamenu">
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+					
+				</ul>
+
+
+
+			</li>
+			<li><a class="mynav" href="#">winter</a>
+				<ul class="megamenu">
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+					
+				</ul>
+
+
+
+			</li>
+			<li><a class="mynav" href="#">sports</a>
+				<ul class="megamenu">
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+
+					<li>
+						<h1>tittle</h1>
+						<a href="#">submenu 1</a>
+						<a href="#">submenu 2</a>
+						<a href="#">submenu 3</a>
+						<a href="#">submenu 4</a>
+						
+					</li>
+					
+				</ul>
+
+
+
+			</li>
+		</ul>
+	</div><br><br><br>
+
+
+
+	
+
+
+
+ 	<style type="text/css">
+ 		.image1{
+ 			margin-top: 300px;
+ 		}
+ 	</style>
+
+	 <div class="image1">
+		<section class="container">
+		<div class="bd-example">
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="image/1.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Loafer Shoe</h5>
+          <p>fully comfortable shoe for Boys N Girls</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="image/2.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>formal Shoe</h5>
+          <p>Fit H Boss</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="image/1.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Casual shoe</h5>
+          <p> Just Feel </p>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+</section> 
+	</div>
+
+
+	<!-- image -->
+<section>
+	
+
+</section>
+
+`
+	<!-- Footer -->
+
+
+	<section>
+	<div>
+		<!-- Footer -->
+<footer class="page-footer font-small unique-color-dark">
+
+  <div style="background-color:grey;">
+    <div class="container">
+
+      <!-- Grid row-->
+      <div class="row py-4 d-flex align-items-center">
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+          <h6 class="mb-0" style="font-size: 25PX"> DEVLOPED BY PRIYANSHU PRINCE</h6>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left mt-5">
+
+    <!-- Grid row -->
+    <div class="row mt-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+        <!-- Content -->
+        <h6 class="text-uppercase font-weight-bold">Company name</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>Addidas</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Useful links</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a href="#!">Your Account</a>
+        </p>
+        <p>
+          <a href="#!">Become an Affiliate</a>
+        </p>
+        <p>
+          <a href="#!">Shipping Rates</a>
+        </p>
+        <p>
+          <a href="#!">Help</a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Contact</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <i class="fas fa-home mr-3"></i>Sankalp-6 clg Hostel</p>
+        <p>
+          <i class="fas fa-envelope mr-3"></i>priyanshuprinceblogspot.com</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i>9304042116</p>
+        <p>
+          <i class="fas fa-print mr-3"></i>8051858607</p>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+ 
+</footer>
+<!-- Footer -->
+	</div>
+</section>
+
+</body>
+</html>
